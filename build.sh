6a162c215addf6402461f7ef5ea4cb4cb3806c7a
ImSpiDy <SpiDy2713@gmail.com>
2023-01-14 10:51:20 +0000

    Gapps build plox | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ lunch bliss_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
+export BLISS_BUILD_VARIANT=gapps
 
 compile_plox () {
 blissify -g lavender -j10
