d6ed471615c02608eb702e637f8d20ff14b610a0
ImSpiDy <spidy2713@gmail.com>
2023-01-15 22:50:01 +0000

    make kela Rom plox
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch banana_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export WITH_GAPPS=true
-#export TARGET_GAPPS_ARCH=arm64
+export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 #export BLISS_BUILD_VARIANT=gapps
 
 compile_plox () {
-make bacon -j16
+mka bacon -j16
 }
