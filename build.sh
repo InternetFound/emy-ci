4124e2fcd400500315d1ad46c201ab77d571f4bd
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-01-15 16:01:44 +0530

    Let's see | V

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ lunch corvus_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_DUP_RULES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 compile_plox () {
 make corvus -j10
