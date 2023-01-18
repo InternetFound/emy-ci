c3895ad65bba2eb4240c1f2ea1c117a5e7dd1c1e
Prashant-1695 <prashant33968@gmail.com>
2023-01-18 18:56:41 +0530

    Build | V

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 # Normal build steps
 . build/envsetup.sh
-lunch corvus_lavender-userdebug
+lunch p404_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 compile_plox () {
-make corvus -j10
+make bacon -j10
 }
