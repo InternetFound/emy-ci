55b3720f05d091d0b1a381a6ff1b072bb9240663
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-01-20 17:58:58 +0530

    ci: call audio fix | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ lunch aospa_lavender-user
 export TZ=Asia/Kolkata
 
 compile_plox () {
-./rom-build.sh lavender -t user -s keys -j10
+./rom-build.sh lavender -t user -s keys -j14
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
