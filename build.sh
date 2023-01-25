b8ffed800916d1ecbf051e5bfefeb84703674a8b
Prashant-1695 <prashant33968@gmail.com>
2023-01-25 17:27:29 +0530

    build | V

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch derp_lavender-userdebug
+lunch superior_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export SUPERIOR_GAPPS=minimal
 
 compile_plox () {
-mka derp -j10
+m bacon -j10
 }
