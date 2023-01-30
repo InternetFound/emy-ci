46f7eac59e8e2b430a33f6f21db4c759a511aee0
ImSpiDy <SpiDy2713@gmail.com>
2023-01-30 10:09:52 +0000

    Arrow 4.4 A13 | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ lunch arrow_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export SUPERIOR_GAPPS=minimal
+export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 
 compile_plox () {
 m bacon -j17
