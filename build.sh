ec67fa5f78305dcef0da5a8ab0f2befcd33fb84d
ImSpiDy <SpiDy2713@gmail.com>
2023-01-30 15:46:48 +0000

    make both variants | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,11 +2,19 @@
 . build/envsetup.sh
 lunch arrow_lavender-userdebug
 
+# 0 = Vanilla or Gapps
+# 1 = Vanilla and Gapps
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+
+exp_gapps() {
 export WITH_GAPPS=true
+export ARROW_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
+}
 
 compile_plox () {
 m bacon -j17
