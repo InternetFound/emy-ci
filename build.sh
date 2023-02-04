870520f5f4a9004943eac28a4586df9e9c31e4fe
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-02-04 22:34:55 +0530

    ci: fu cirrus | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ lunch aospa_lavender-user
 export TZ=Asia/Kolkata
 
 compile_plox () {
+ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
 ./rom-build.sh lavender -t user -s keys -j16
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
