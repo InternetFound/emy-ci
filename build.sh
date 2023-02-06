8cf9584a35f50849e3d503977b264556d0abb413
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-02-07 01:47:12 +0530

    userdebug moment | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 # Normal build steps
 . build/envsetup.sh
-lunch aospa_lavender-user
+lunch aospa_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
+export SELINUX_IGNORE_NEVERALLOWS=true
 
 compile_plox () {
 ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
-./rom-build.sh lavender -t user -s keys -j16
+./rom-build.sh lavender -t userdebug -s keys -j16
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
