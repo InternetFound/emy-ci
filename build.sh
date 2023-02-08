fc35f8c99f780d2738cb0444c0ddbb70d8d46345
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-02-08 06:23:03 +0530

    k cirrus | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 
 compile_plox () {
 ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
-ls /tmp/rom/out/target/product/lavender/vendor.img || make vendorimage -j12
+#ls /tmp/rom/out/target/product/lavender/vendor.img || make vendorimage -j12
 ./rom-build.sh lavender -t userdebug -s keys -j16
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
