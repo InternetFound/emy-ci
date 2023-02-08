0ae400a960564c1b93f59a45c511fe13448a74a2
ImSpiDy <SpiDy2713@gmail.com>
2023-02-08 08:36:35 +0000

    make xd
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch xdroid_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
@@ -8,5 +8,5 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 #export ARROW_GAPPS=true
 
 compile_plox () {
-make nad -j$(nproc --all)
+make xd -j17
 }
