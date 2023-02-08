d41a3390a22ff28e88e6c0a1fb0dfb66b8ddbc4c
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-02-08 08:47:03 +0530

    kk | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ compile_plox () {
 ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
 #ls /tmp/rom/out/target/product/lavender/vendor.img || make vendorimage -j12
 ./rom-build.sh lavender -t userdebug -s keys -j16
+ls /tmp/rom/aospa*.zip || ./rom-build.sh lavender -t userdebug -s keys -j16
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
