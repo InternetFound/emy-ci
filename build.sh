82831e31ed599721216ae9be377c7c0570c8ef50
ImSpiDy <SpiDy2713@gmail.com>
2023-02-16 08:58:03 +0000

    cherisshOS 13
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 # Normal build steps
 . build/envsetup.sh
-lunch banana_lavender-userdebug
+lunch cherish_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
-export TARGET_GAPPS_ARCH=arm64
+#export WITH_GAPPS=true
+#export TARGET_GAPPS_ARCH=arm64
 #export BLISS_BUILD_VARIANT=gapps
 
 compile_plox () {
-mka bacon -j32
+mka bacon -j17
 }
