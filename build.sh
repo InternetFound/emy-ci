f181e95d3136891077e0d1df6b6ceb3142010c66
ImSpiDy <SpiDy2713@gmail.com>
2023-02-17 02:05:51 +0530

    make final build | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,12 +2,18 @@
 . build/envsetup.sh
 lunch cherish_lavender-userdebug
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export WITH_GAPPS=true
-#export TARGET_GAPPS_ARCH=arm64
-#export BLISS_BUILD_VARIANT=gapps
+export CHERISH_VANILLA=true
+
+exp_gapps() {
+export CHERISH_VANILLA=false
+export TARGET_USES_MINI_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
+}
 
 compile_plox () {
 mka bacon -j17
