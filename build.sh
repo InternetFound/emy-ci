9c7573e5a6e6b9d7ea7509728911acba38901f11
ImSpiDy <SpiDy2713@gmail.com>
2023-02-19 15:01:42 +0530

    plox make haboc 10
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch havoc_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 #export ARROW_GAPPS=true
 
 compile_plox () {
-make nad -j$(nproc --all)
+mka bacon -j$(nproc --all)
 }
