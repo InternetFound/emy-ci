f18d83a042485191508c9fb614895d4ab279f18c
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-02-22 17:35:57 +0530

    Trigger | R

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 . build/envsetup.sh
 lunch conquer_lavender-userdebug
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_USES_OLD_DISPLAY_HAL=true
+
+exp_gapps() {
+export TARGET_USE_GAPPS=true
+}
 
 compile_plox () {
 make carthage -j10
